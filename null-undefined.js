f3c339a9b740ce6fa653e7e44f6aba27ff7fe0d5
asif2703/ git config --global user.name asif2703 <wapasif@gmail.com>
2020-08-08 17:08:13 +0600

    complete null-undefined

---
+++ null-undefined.js
@@ -0,0 +1,23 @@
+///undefined cause 
+let pakhi;
+console.log(pakhi);
+function add(num1,num2) {
+    console.log(num1+num2);
+    // return add; [if not call to return or only return]
+}
+const result = add(5,7);
+console.log(result);
+
+function add(num1,num2) {
+    console.log(num2);
+}
+// //const result1 = add(12);[not define to num2 value so its undefined];
+// console.log(result1);
+
+const premik = { name:"sala romi" , phone: "45632"};
+console.log(premik.gf);
+
+let fun = undefined;
+console.log(undefined);
+
+// null mean not define...must be set to null ex- let fun = null;
