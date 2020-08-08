b3a8dbf04b6b4ac730ea255affa0a461cbe9f4a9
asif2703/ git config --global user.name asif2703 <wapasif@gmail.com>
2020-08-08 16:38:38 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,17 @@
+//falsy value
+// 0;
+//false
+// null;
+// undefined;
+// '';
+// NaN;
+// truthy value
+// "0", " ","false" ,[],
+const name = '';
+
+if (name.length >0) {
+    console.log('condition is true');
+}
+else{
+    console.log('condition is false')
+}
