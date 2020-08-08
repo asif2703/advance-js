752241b9dee18a4386fc980d959ae9f18c5feab8
asif2703/ git config --global user.name asif2703 <wapasif@gmail.com>
2020-08-08 17:29:38 +0600

    complete double and triple equal difference

---
+++ doubleVsTriple.js
@@ -0,0 +1,19 @@
+let num = 10;
+let num1 ='10';
+if (num == num1) {
+    console.log('condition is true');
+}
+else{
+    console.log('condition is false');
+}
+let num2 = 10;
+let num3 ='10';
+if (num2 === num3) {
+    console.log('condition is true');
+}
+
+else {
+    console.log('condition is false');
+}  
+
+// note: "double equal(==)"al not check which type of value but "triple equal(===)" check the value. 
